338069f6c6cf7f5b95598a1670e8400d387a09ff
neeraja <nerys123@gmail.com>
2022-10-19 06:51:31 -0400

    Naveen's program comitted

---
+++ src/main/java/com/learning/automation/javalearning/neeraja/ArthimeticOperator.java
@@ -0,0 +1,11 @@
+package com.learning.automation.javalearning.neeraja;
+
+public class ArthimeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Arthimetic operation program");
+
+	}
+
+}
